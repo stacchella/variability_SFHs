5ce12db56a1328450e4de992b58f775ff4fff6dd
stacchella <sandro.tacchella@cfa.harvard.edu>
2018-10-16 17:30:29 +0200

    added lum computation

--- scripts/run_make_SFH.py
+++ scripts/run_make_SFH.py
@@ -21,6 +21,8 @@ import os
 import h5py
 import argparse
 import make_SFH_from_PSD
+import fsps
+import compute_luminosities
 
 
 from astropy.cosmology import FlatLambdaCDM
@@ -42,21 +44,36 @@ parser.add_argument("--filename_SFH", type=str, help="filename of SFH file")
 parser.add_argument("--sfh_res", type=float, help="time resolution of SFH in Gyr")
 parser.add_argument("--redshift_start", type=float, help="start of SFH in redshift space")
 parser.add_argument("--redshift_end", type=float, help="end of SFH in redshift space")
+parser.add_argument("--redshift_lum_start", type=float, help="start of luminosity computation in redshift space")
+parser.add_argument("--redshift_lum_end", type=float, help="end of luminosity computation in redshift space")
 parser.add_argument("--number_galaxies", type=int, help="Number of SFHs/galaxies")
 parser.add_argument("--scatter_MS_0", type=float, help="scatter of MS (on shortest timescale)")
 parser.add_argument("--list_of_slopes", nargs='+', type=float, help="slope on the short time scales")
 parser.add_argument("--list_of_breaks", nargs='+', type=float, help="timescale of break")
 parser.add_argument("--aliasTbin", type=float, help="oversampling of time series (resolution)")
+parser.add_argument("--logzsol", type=float, help="metallicity: logZsol in FSPS")
+parser.add_argument("--dust2", type=float, help="dust2 in FSPS")
+
 args = parser.parse_args()
 
 
 # get grid of times
 
-time_SFH = np.arange(cosmo.age(args.redshift_start).value, cosmo.age(args.redshift_end).value, args.sfh_res)
-number_steps = len(time_SFH)  # each step coresponds to 1.0 Myr, so total is 10 Gyr
+time_cosmo = np.arange(cosmo.age(args.redshift_start).value, cosmo.age(args.redshift_end).value, args.sfh_res)
+number_steps = len(time_cosmo)
 print('number of time steps: ' + str(number_steps))
 print('number of galaxies: ' + str(args.number_galaxies))
 
+time_SFH = time_cosmo-time_cosmo[0]
+print time_SFH
+print len(time_SFH)
+
+# get time of luminosity
+
+t_start = cosmo.age(args.redshift_lum_start).value-time_cosmo[0]
+t_end = cosmo.age(args.redshift_lum_end).value-time_cosmo[0]
+idx_t = (time_SFH >= t_start) & (time_SFH <= t_end)
+time_lum = time_SFH[idx_t]
 
 ### set parameters of light curve model ###
 # amplitude => seems not to work
@@ -96,6 +113,27 @@ ii_slope, jj_break = translate_key(args.idx_key)
 array_of_SFH = make_SFH_from_PSD.create_family_SFHs(args.number_galaxies, list_of_slopes[ii_slope], list_of_breaks[jj_break], args.scatter_MS_0, args.aliasTbin, number_steps, amp, slope_high)
 
 
+# compute luminosities
+
+# set up fsps
+sp = fsps.StellarPopulation(compute_vega_mags=False, zcontinuous=1, imf_type=1, add_neb_emission=True, sfh=3, logzsol=args.logzsol, dust_type=2, dust2=args.dust2)
+
+
+# define filters
+filters = ['i1500', 'i2800', 'u', 'v', '2mass_j', 'wise_w3']
+idx_Halpha = (np.abs(sp.emline_wavelengths-6564.61)).argmin()
+
+
+array_of_luminosities = []
+
+for ii in range(number_galaxies):
+    SFR = 10**array_of_SFH[ii]  # assume MS is 1 Msun/yr
+    if (ii == 0):
+        array_of_luminosities = compute_luminosities.get_magnitude_SFH(sp, time_SFH, SFR, time_lum, filters, idx_Halpha)
+    else:
+        array_of_luminosities = np.vstack([array_of_luminosities, compute_luminosities.get_magnitude_SFH(sp, time_SFH, SFR, time_lum, filters, idx_Halpha)])
+
+
 # save SFH dictionary (contains SFH)
 file_name = path_SFH_cat + args.filename_SFH + 'alpha_' + list_of_slopes_str[ii_slope] + '_tau_' + list_of_breaks_str[jj_break] + '.hdf5'
 
@@ -107,8 +145,12 @@ except OSError:
 f = h5py.File(file_name, 'w')
 # add SFH
 grp_SFH = f.create_group("SFH")
+grp_SFH.create_dataset('SFH_time_cosmo', data=time_cosmo)
 grp_SFH.create_dataset('SFH_time', data=time_SFH)
 grp_SFH.create_dataset('SFH_SFR', data=array_of_SFH)
+grp_lum = f.create_group("lum")
+grp_lum.create_dataset('lum_time', data=time_lum)
+grp_lum.create_dataset('lum_luminosities', data=array_of_luminosities)
 
 f.close()
 
---
+++ scripts/variability_tools/compute_luminosities.py
@@ -0,0 +1,120 @@
+# coding: utf-8
+
+#!/usr/bin/env python2
+
+"""
+Created on October 17, 2018
+
+@author: sandro.tacchella@cfa.harvard.edu
+
+"""
+
+import numpy as np
+from scipy.interpolate import interp1d
+from scipy.integrate import simps
+import fsps
+
+from astropy.cosmology import FlatLambdaCDM
+cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
+print cosmo
+
+
+def integrate_mag(spec_lam, spectra, filter, z=None):
+    '''
+    borrowed from calc_ml
+    given a filter name and spectrum, calculate magnitude/luminosity in filter (see alt_file for filter names)
+    INPUT:
+        SPEC_LAM: must be in angstroms. this will NOT BE corrected for reddening even if redshift is specified. this
+        allows you to calculate magnitudes in rest or observed frame.
+        SPECTRA: must be in Lsun/Hz (FSPS standard). if redshift is specified, the normalization will be taken care of.
+    OUTPUT:
+        MAG: comes out as absolute magnitude
+        LUMINOSITY: comes out in erg/s
+            NOTE: if redshift is specified, INSTEAD RETURN apparent magnitude and flux [erg/s/cm^2]
+    '''
+    resp_lam = filter[0][0]
+    res = filter[1][0]
+    # physical units, in CGS
+    pc2cm = 3.08568E18
+    lsun = 3.839E33
+    c = 2.99E10
+    # interpolate filter response onto spectral lambda array
+    # when interpolating, set response outside wavelength range to be zero.
+    response_interp_function = interp1d(resp_lam, res, bounds_error=False, fill_value=0)
+    resp_interp = response_interp_function(spec_lam)
+    # integrate spectrum over filter response
+    # first calculate luminosity: convert to flambda (factor of c/lam^2, with c converted to AA/s)
+    # then integrate over flambda [Lsun/AA] to get Lsun
+    spec_flam = spectra*(c*1e8/(spec_lam**2))
+    luminosity = simps(spec_flam*resp_interp, spec_lam)
+    # now calculate luminosity density [erg/s/Hz] in filter
+    # this involves normalizing the filter response by integrating over wavelength
+    norm = simps(resp_interp/spec_lam, spec_lam)
+    luminosity_density = simps(spectra*(resp_interp/norm)/spec_lam, spec_lam)
+    # if redshift is specified, convert to flux and apparent magnitude
+    if z is not None:
+        dfactor = (cosmo.luminosity_distance(z).value*1e5)**(-2)*(1+z)
+        luminosity = luminosity*dfactor
+        luminosity_density = luminosity_density*dfactor
+    # convert luminosity density to flux density
+    # the units of the spectra are Lsun/Hz; convert to
+    # erg/s/cm^2/Hz, at 10pc for absolute mags
+    flux_density = luminosity_density*lsun/(4.0*np.pi*(pc2cm*10)**2)
+    luminosity = luminosity*lsun
+    # convert flux density to magnitudes in AB system
+    mag = -2.5*np.log10(flux_density)-48.60
+    #print 'maggies: {0}'.format(10**(-0.4*mag)*1e10)
+    return mag, luminosity
+
+
+def return_lir(lam, spec, z=None):
+    """ returns IR luminosity (8-1000 microns) in erg/s
+    input spectrum must be Lsun/Hz, wavelength in \AA
+    """
+    botlam = np.atleast_1d(8e4-1)
+    toplam = np.atleast_1d(1000e4+1)
+    edgetrans = np.atleast_1d(0)
+    lir_filter = [[np.concatenate((botlam, np.linspace(8e4, 1000e4, num=100), toplam))],
+                  [np.concatenate((edgetrans, np.ones(100), edgetrans))]]
+    # calculate integral
+    _, lir = integrate_mag(lam, spec, lir_filter, z=z)  # comes out in ergs/s
+    return lir
+
+
+def return_luv(lam, spec, z=None):
+    """ returns UV luminosity (1216-3000 \AA) in erg/s
+    input spectrum must be Lsun/Hz, wavelength in \AA
+    """
+    botlam = np.atleast_1d(1216)
+    toplam = np.atleast_1d(3000)
+    edgetrans = np.atleast_1d(0)
+    luv_filter = [[np.concatenate((botlam-1, np.linspace(botlam, toplam, num=100), toplam+1))],
+                  [np.concatenate((edgetrans, np.ones(100), edgetrans))]]
+    # calculate integral
+    _, luv = integrate_mag(lam, spec, luv_filter, z=z)  # comes out in ergs/s
+    return luv
+
+
+def get_magnitude_SFH(sp_in, time, SFR, tage_list, filters, idx_Halpha):
+    for tage in tage_list:
+        L_list = get_magnitude_SFH_single(sp_in, time, SFR, tage, filters, idx_Halpha)
+        if (tage == tage_list[0]):
+            L_mat = L_list
+        else:
+            L_mat = np.vstack([L_mat, L_list])
+    return(L_mat)
+
+
+def get_magnitude_SFH_single(sp_in, time, SFR, tage_in, filters, idx_Halpha):
+    # set SFH parameters
+    sp_in.set_tabular_sfh(time, SFR)
+    # get luminosities
+    mag_list = sp_in.get_mags(tage=tage_in, bands=filters)
+    L_list = 4*np.pi*(3.086e+19)**2*np.power(10, -0.4*(mag_list+48.6))
+    wave, spec = sp_in.get_spectrum(tage=tage_in)
+    L_tot = return_lir(wave, spec, z=None) + 2.2*return_luv(wave, spec, z=None)  # from total UV
+    L_tot2 = return_lir(wave, spec, z=None) + 2.2*1.5*L_list[1]*3e8/(2800*10**-10)  # from 2800
+    L_list = np.append(np.append(np.append(L_list, L_tot), L_tot2), 3.839*10**33*sp_in.emline_luminosity[idx_Halpha])
+    return(L_list)
+
+
